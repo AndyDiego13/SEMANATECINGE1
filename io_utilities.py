2555340a2ce83ef0c651fd57b13a91b5adb2c9da
Manzanis <57538809+Manzanis@users.noreply.github.com>
2020-09-18 12:21:20 -0500

    Add files via upload

---
+++ io_utilities.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def load_data(filename):
+
+    with open(filename,'r') as fp:
+        data = fp.read().split('\n')
+    data_new = [f.split(',') for f in data if f != ""]
+    data_formatted = []
+    for instance in data_new:
+        instance_new = []
+        for value in instance:
+            try:
+                instance_new.append(float(value))
+            except ValueError:
+                instance_new.append(value)
+        data_formatted.append(instance_new)
+    return data_formatted
