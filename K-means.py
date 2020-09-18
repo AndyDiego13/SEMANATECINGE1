d04fed20c51821cc22069fb193d145dae707b6da
Manzanis <57538809+Manzanis@users.noreply.github.com>
2020-09-18 12:20:32 -0500

    Update K-means.py

--- K-means.py
+++ K-means.py
@@ -1,39 +1,63 @@
 import numpy as np
+import random
+import matplotlib as plt
+
+from io_utilities import load_data
+from visualizations import show_clusters_centroids
 
 def dist(a, b):
-    c = 0
-
-    for i in range(len(a)):
-        c += (b[i] - a[i])**2
+    c = np.sum((np.array(b) - np.array(a))**2)
     return np.sqrt(c)
+
 
 def cerc(puntos, centros):
     a = [[] for f in centros]
 
     for i, punto in enumerate(puntos):
-        this_distance = []
-        for j,centro in enumerate(centros):
-            this_distance.append(dist((punto, centro)))
-        this_argmin = np.argmin(this_distance)
-        a[this_argmin].append(punto)
+        distnc = []
+
+        for j, centro in enumerate(centros):
+            #import pdb; pdb.set_trace()
+            distnc.append(dist(punto, centro))
+        argmin = np.argmin(distnc)
+
+        a[argmin].append(punto)
 
     return a
 
 
 def centros(a):
     b = []
-    c= 0
-    x = np.array()
 
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            x.append(np.array(a[i][j]))    #[[x, y, z], [x, y, z]] x = [x1, x2, xn]
-        c = c/len(a[i])
-        b.append(c)
+    for a in a:
+        centroid = np.mean(a, axis = 0)
+        b.append(centroid)
+
     return b
 
 
-def k_means(puntos):
-    k = np.random.rand()
+def k_means(puntos, k, iter = 10):
 
-    puntos.append(k)
+    indx = np.random.randint(len(puntos), size = k)
+
+    centroids = puntos[indx, :]
+    clusters = cerc(puntos, centroids)
+
+    for i in range(iter):
+        clusters = cerc(puntos, centroids)
+        centroids = centros(clusters)
+
+    return clusters, centroids
+
+if __name__ == "__main__":
+
+    data = load_data('iris.data')
+    k = 3
+
+    x = np.array([f[:-1] for f in data])
+    y = np.array([f[-1] for f in data])
+
+    clusters, centroids = k_means(x, k)
+
+    show_clusters_centroids(clusters, centroids, "Result", keep = True)
+    plt.show()
